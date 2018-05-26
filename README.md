526900452aff81cccc9727591da064d8ce4ab5b7
Yadiel <ycabr035@fiu.edu>
2018-05-26 16:25:51 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<strong>TicTacTuring: A Full Stack ReactJs Application</strong>
+
+Author: Yadiel Cabrera<br>
+URL: http://yadielcabrera.com<br>
+
+Description: <br>
+This is a full stack react application.
+edd
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <div>
+  <h1>Hello World!</h1> 
+  </div>, document.getElementById('root')
+);
+registerServiceWorker();

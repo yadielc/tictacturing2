f0ea17c78cbab3d0d6de8dd26990e09a218592ac
Yadiel <ycabr035@fiu.edu>
2018-05-26 19:04:00 -0400

    edited home and profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Route, IndexRoute} from 'react-router'
 import Template from '../containers/Template'
+import Home from '../containers/Home'
+import Profile from '../containers/Profile'
 
 const createRoutes = () => {
   return (
@@ -8,7 +10,13 @@ const createRoutes = () => {
       path='/'
       component={Template}
     >
-
+      <IndexRoute
+        component={Home}
+      />
+      <Route
+        path={'/profile'}
+        component={Profile}
+      />
     </Route>
   )
 }

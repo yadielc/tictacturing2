81d3f5e69e00acfb9ca316de1b0cf4c9cb3e95ce
Yadiel <ycabr035@fiu.edu>
2018-05-27 19:26:14 -0400

    added squares

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Stage} from 'react-konva'
-import {Board} from '../styled/TicTacToe'
+import {Board, Squares} from '../styled/TicTacToe'
 
 class TicTacToe extends Component {
 
@@ -21,15 +21,23 @@ class TicTacToe extends Component {
     let size = (height < width) ? height * .8 : width * .8
     let rows = this.state.rows
     let unit = size / rows
+    let coordinates = []
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < rows; x++) {
+        coordinates.push([x*unit, y*unit])
+      }
+    }
 
     this.setState({
       size,
       rows,
-      unit
+      unit,
+      coordinates
     })
   }
 
-  move = () => {
+  move = (marker, index) => {
+    console.log('Move made', marker, index)
     //placeholder
   }
 
@@ -49,7 +57,13 @@ class TicTacToe extends Component {
     let {
       size,
       unit,
-      rows
+      rows,
+      coordinates,
+      gameState,
+      win,
+      gameOver,
+      yourTurn,
+      ownMark
     } = this.state
     return (
       <div>
@@ -62,7 +76,16 @@ class TicTacToe extends Component {
             rows={rows}
             size={size}
           />
-          {/*<Squares/>*/}
+          <Squares
+            unit={unit}
+            coordinates={coordinates}
+            gameState={gameState}
+            win={win}
+            gameOver={gameOver}
+            yourTurn={yourTurn}
+            ownMark={ownMark}
+            move={this.move}
+          />
         </Stage>
       </div>
     )
--- src/styled/TicTacToe.js
+++ src/styled/TicTacToe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Layer, Line} from 'react-konva'
+import {Layer, Line, Text} from 'react-konva'
 
 export const Board = ({unit, size, rows}) => {
 	let grid = []
@@ -27,6 +27,53 @@ export const Board = ({unit, size, rows}) => {
 	return (
 		<Layer>
 			{grid}
+		</Layer>
+	)
+}
+
+export const Squares = ({
+	unit,
+	coordinates,
+	gameState,
+	win,
+	gameOver,
+	yourTurn,
+	ownMark,
+	move
+}) => {
+	let squares = coordinates.map( (position, index) => {
+			let makeMove = move
+			let mark = gameState[index]
+			let fill = 'black'
+			if (win && win.includes(index)) {
+				fill = 'lightgreen'
+			}
+			if (gameOver || !yourTurn || mark) {
+				makeMove = () => console.log('nope!')
+			}
+			return (
+				<Text
+					key={index}
+					index={index}
+					x={position[0]}
+					y={position[1]}
+					fontSize={unit}
+					width={unit}
+					text={mark}
+					fill={fill}
+					fontFamily={'Helvetica'}
+					align={'center'}
+					onClick={(event)=>{
+						let index = event.target.index
+						makeMove(index, ownMark)
+					}}
+				/>
+			)
+	})
+
+	return (
+		<Layer>
+			{squares}
 		</Layer>
 	)
 }

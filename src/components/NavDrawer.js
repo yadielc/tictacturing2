e1684491fc004c1824736c0b7b430828ab96098d
Yadiel <ycabr035@fiu.edu>
2018-05-27 17:13:40 -0400

    added links to NavDrawer component

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -4,6 +4,7 @@ import MenuItem from 'material-ui/MenuItem'
 import Divider from 'material-ui/Divider'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import Menu from 'material-ui/svg-icons/navigation/menu'
+import {Link} from 'react-router'
 
 
 class NavDrawer extends Component {
@@ -40,12 +41,22 @@ class NavDrawer extends Component {
               LoginContainer
             </div>
             <Divider/>
-            <MenuItem
-              primaryText={'Play'}
-            />
-            <MenuItem
-              primaryText={'Profile'}
-            />
+            <Link
+              to={'/'}
+            >
+              <MenuItem
+                onTouchTap={this.toggle}
+                primaryText={'Play'}
+              />
+            </Link>
+            <Link
+              to={'/profile'}
+            >
+              <MenuItem
+                onTouchTap={this.toggle}
+                primaryText={'Profile'}
+              />
+            </Link>
           </Drawer>
         </div>
       )

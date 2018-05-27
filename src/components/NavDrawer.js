b0f4283889f0ae1f60a81dbffe5b77b89c956d5c
Yadiel <ycabr035@fiu.edu>
2018-05-26 22:15:58 -0400

    added NavDrawer component

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -5,14 +5,40 @@ import Divider from 'material-ui/Divider'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import Menu from 'material-ui/svg-icons/navigation/menu'
 
+
 class NavDrawer extends Component {
+    state = {
+      open: false
+    }
+
+    toggle = () => {
+      this.setState((prevState, props) => {
+        return {
+          open: !prevState.open
+        }
+      })
+    }
+
     render() {
       return (
         <div>
-          <FloatingActionButton>
+          <FloatingActionButton
+            onTouchTap={this.toggle}
+          >
             <Menu/>
           </FloatingActionButton>
-          <Drawer>
+          <Drawer
+            open={this.state.open}
+          >
+            <div
+              style={{
+                height: '200px',
+                width: '100%',
+                backgroundColor: 'salmon'
+              }}
+            >
+              LoginContainer
+            </div>
             <Divider/>
             <MenuItem
               primaryText={'Play'}
--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import RaisedButton from 'material-ui/RaisedButton'
+import NavDrawer from '../components/NavDrawer'
 
 injectTapEventPlugin()
 
@@ -11,14 +11,7 @@ class Template extends Component {
     return (
       <MuiThemeProvider>
         <div>
-          <header>
-            <h1>TicTacTuring</h1>
-            <RaisedButton
-              label={'Test Button'}
-              primary={true}
-              onTouchTap={()=>{console.log('hello, I work')}}
-            />
-          </header>
+          <NavDrawer/>
           <main>
             {this.props.children}
           </main>

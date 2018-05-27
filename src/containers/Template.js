6aeebb4ad6fef9965514616f206f121f64765105
Yadiel <ycabr035@fiu.edu>
2018-05-26 21:22:53 -0400

    added material ui

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,17 +1,29 @@
 import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import RaisedButton from 'material-ui/RaisedButton'
+
+injectTapEventPlugin()
 
 class Template extends Component {
 
   render() {
     return (
-      <div>
-        <header>
-          <h1>TicTacTuring</h1>
-        </header>
-        <main>
-          {this.props.children}
-        </main>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <header>
+            <h1>TicTacTuring</h1>
+            <RaisedButton
+              label={'Test Button'}
+              primary={true}
+              onTouchTap={()=>{console.log('hello, I work')}}
+            />
+          </header>
+          <main>
+            {this.props.children}
+          </main>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }

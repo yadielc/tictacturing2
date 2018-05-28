bca0494f610320002ed90aa38a9f9007b447ff70
Yadiel <ycabr035@fiu.edu>
2018-05-28 15:18:44 -0400

    added react-relay

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, browserHistory} from 'react-router'
 import Routes from './routes'
+import Relay from 'react-relay'
+import userRelay from 'react-router-relay'
+import {RelayNetworklayer, urlMiddleware} from 'react-relay-networklaayer'
+import {relayApi} from './config/endpoints'
+import auth from './utils/auth'
+
+const createHeaders = () => {
+  let idToken = auth.getToken()
+  if (idToken) {
+    Authorization: `Bearer ${idToken}`
+  } else {
+    return {}
+  }
+}
 
 ReactDOM.render(
   <Router

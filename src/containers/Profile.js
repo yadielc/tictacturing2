7920e2bbe3d187812972c3a63b7bd387b050cec4
Yadiel <ycabr035@fiu.edu>
2018-05-28 14:55:40 -0400

    added profile pages

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -26,6 +26,30 @@ class Profile extends Component {
     }
   }
 
+  get records() {
+    return this.props.user.games.map( (game,index) => {
+      return (
+        <GameRecord
+          key={index}
+          index={index}
+        >
+          <Column>
+            {(game.winner) ? 'Won!' : "Didn't win"}
+          </Column>
+          <Column>
+            "ROBOT"
+          </Column>
+          <Column>
+            "No"
+          </Column>
+          <Column>
+            {game.createdAt}
+          </Column>
+        </GameRecord>
+      )
+    })
+  }
+
   render() {
     let {email} = this.props.user
     return (
@@ -51,6 +75,7 @@ class Profile extends Component {
               Date
             </Column>
           </ColumnLabels>
+          {this.records}
         </GameList>
       </Container>
     )

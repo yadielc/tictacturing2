c36184354d24d1207b94c9f2ea5e44c08f909000
Yadiel <ycabr035@fiu.edu>
2018-05-28 15:12:15 -0400

    added authentication capability

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,2 +1,70 @@
+import Auth0Lock from 'auth0-lock'
 const authDomain = 'ysites.auth0.com'
 const clientId = 'PnqB7NfjvfOyV0xi0694HImEizzwEVVh'
+
+class AuthService {
+	constructor() {
+		this.lock = new Auth0Lock(clientId, authDomain, {
+			auth: {
+				params: {
+					scope: 'openid email'
+				},
+			},
+		})
+
+		this.showLock = this.showLock.bind(this)
+
+		this.lock.on('authenticated', this.authProcess.bind(this))
+	}
+
+	authProcess = (authResult) => {
+		console.log(authResult)
+	}
+
+	showLock() {
+		this.lock.show()
+	}
+
+	setToken = (authFields) => {
+		let {
+			idToken,
+			exp
+		} = authFields
+		localStorage.setItem('idToken', idToken)
+		localStorage.setItem('exp', exp * 1000)
+	}
+
+	isCurrent = () => {
+		let expString = localStorage.getItem('exp')
+		if (!expString) {
+			localStorage.removeItem('idToken')
+			return false
+		}
+		let now = new Date()
+		let exp = new Date(parseInt(expString, 10)) //10 is radix parameter
+		if ( exp < now ) {
+			this.logout()
+			return false
+		} else {
+			return true
+		}
+	}
+
+	getToken() {
+		let idToken = localStorage.getItem('idToken')
+		if (this.isCurrent() && idToken) {
+			return idToken
+		} else {
+			localStorage.removeItem('idToken')
+			localStorage.removeItem('exp')
+			return false
+		}
+	}
+
+	logout = () => {
+		localStorage.removeItem('idToken')
+		localStorage.removeItem('exp')
+		location.reload()
+	}
+
+}
